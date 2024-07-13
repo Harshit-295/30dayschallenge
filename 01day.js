c65253a04f3cdfddfd5f942dd9f4e05e166cf11e
Harshit-295 <bhardwajharsh1984@gmail.com>
2024-07-13 14:29:21 +0530

    Add files via upload

---
+++ 01day.js
@@ -0,0 +1,43 @@
+// activity 1
+
+// var a=10;
+// let name="harshit";
+
+// console.log(a);
+// console.log(name);
+
+// // Activity 2
+// const b=true;
+// console.log(b);
+
+//Activity 3
+
+// let c=20;
+// let greet="harshit"
+// let god=true;
+
+// const details={
+//     address:"house no 61",
+//     email:"heloo@gamil.com"
+// }
+
+// const myArr=[1,2,3,4];
+
+// console.log(typeof c);
+// console.log(typeof greet);
+// console.log(typeof god);
+// console.log(typeof details);
+// console.log(typeof myArr);
+
+
+//Activity 4
+
+let oldValue=4;
+oldValue=20;
+console.log(oldValue);
+console.log(oldValue);
+
+// Activity 5
+const a=20;
+a=30;
+console.log(a);
